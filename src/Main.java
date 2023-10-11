6e56af6aa49a80b78efcfdba5707e15a1916db6d
Norayr <asoyannorayr99@gmail.com>
2023-10-11 20:26:20 +0300

    add commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public static boolean doubleExpression(double a, double b, double c) {
+        boolean x = Math.abs((a + b) - c) < 1E-4;
+        return x;
+    }
+}
